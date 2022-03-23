1793aa775311efd812f122d8b1d36355463c2b9e
Vitek3573 <vitya_fetisov@mail.ru>
2022-03-23 22:10:24 +0300

    create adaptiv header

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,12 @@
 $(function(){
-    var mixer = mixitup('.portfolio__content');
+   
     $('.slider-blog__inner').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>'
     });
+    $('.menu__btn').on('click', function(){
+        $('.header__top-inner').toggleClass('header__top-inner--active')
+    });
+    var mixer = mixitup('.portfolio__content');
 });
