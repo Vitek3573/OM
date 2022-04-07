1152dbbe8ff7321538e630e6b5d846c4d7c07adc
Vitek3573 <vitya_fetisov@mail.ru>
2022-04-07 20:27:13 +0300

    create all project

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
-$(function(){
-   
-    $('.slider-blog__inner').slick({
+$(function () {
+  $(".menu a, .go-top").on("click", function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+  $('.slider-blog__inner').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
@@ -8,13 +14,13 @@ $(function(){
       {
         breakpoint: 768,
         settings: {
-            arrows:false
+          arrows: false
         }
       },
     ]
-    });
-    $('.menu__btn').on('click', function(){
-        $('.header__top-inner').toggleClass('header__top-inner--active')
-    });
-    var mixer = mixitup('.portfolio__content');
+  });
+  $('.menu__btn, .menu a').on('click', function () {
+    $('.header__top-inner').toggleClass('header__top-inner--active')
+  });
+  var mixer = mixitup('.portfolio__content');
 });
